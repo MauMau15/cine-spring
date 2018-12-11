4a5555c05caf706d11ba04b37d5889f2c9cbd3b2
MauMau15 <mauriciorenero@gmail.com>
2018-12-11 01:18:29 -0600

    adding a list of Pelicula

--- src/main/java/net/itinajero/app/controller/HomeController.java
+++ src/main/java/net/itinajero/app/controller/HomeController.java
@@ -1,5 +1,7 @@
 package net.itinajero.app.controller;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -7,6 +9,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import net.itinajero.app.model.Pelicula;
 
 @Controller
 public class HomeController {
@@ -18,12 +22,43 @@ public class HomeController {
 	
 	@RequestMapping(value="/", method=RequestMethod.GET)
 	public String mostrarPrincipal(Model model) {
-		List<String> peliculas = new LinkedList<>();
+		/*List<String> peliculas = new LinkedList<>();
 		peliculas.add("Rápidos y furiosos");
 		peliculas.add("El aro 2");
 		peliculas.add("Aliens");
 		
-		model.addAttribute("peliculas", peliculas);
+		model.addAttribute("peliculas", peliculas);*/
+		
+		SimpleDateFormat format = new SimpleDateFormat("dd-MM-yyyy");
+		List<Pelicula> peliculas = null;
+		Pelicula peli1 = null, peli2 = null;
+		try {
+			peliculas = new LinkedList<>();
+			
+			peli1 = new Pelicula();
+			peli1.setId(1);
+			peli1.setTitulo("El aro 2");
+			peli1.setFechaEstreno(format.parse("28-05-2018"));
+			peli1.setDuracion(152);
+			peli1.setGenero("terror");
+			peli1.setClasificacion("C");
+			
+			peli2 = new Pelicula();
+			peli2.setId(1);
+			peli2.setTitulo("Aliens");
+			peli2.setFechaEstreno(format.parse("18-06-2008"));
+			peli2.setDuracion(102);
+			peli2.setGenero("infantil");
+			peli2.setClasificacion("A");
+			
+			peliculas.add(peli1);
+			peliculas.add(peli2);
+			
+			model.addAttribute("peliculas",peliculas);
+		}catch(ParseException e) {
+			System.out.println(e);
+		}
+		
 		return "/home";
 	}
 	
