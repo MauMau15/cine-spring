22b95532402ff37850271cca127916234c2447c5
MauMau15 <mauriciorenero@gmail.com>
2018-12-10 23:16:32 -0600

    adding pelicula detail and learnig Model class

--- src/main/java/net/itinajero/app/controller/HomeController.java
+++ src/main/java/net/itinajero/app/controller/HomeController.java
@@ -1,6 +1,7 @@
 package net.itinajero.app.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -10,5 +11,18 @@ public class HomeController {
 	@RequestMapping(value="/home", method=RequestMethod.GET)
 	public String getHome() {
 		return "/home";
+	}
+	
+	@RequestMapping(value="/detail")
+	public String mostrarDetalle(Model model) {
+		String tituloPelicula = "Rápidos y furiosos";
+		int duracion = 136;
+		double precio = 50;
+		
+		model.addAttribute("titulo", tituloPelicula);
+		model.addAttribute("duracion", duracion);
+		model.addAttribute("precio", precio);
+		
+		return "/detalle";
 	}
 }
