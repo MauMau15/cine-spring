bdeb0ddcc39c45520007eae7d9b9b61402a72648
MauMau15 <mauriciorenero@gmail.com>
2018-12-14 16:59:31 -0600

    adding script for tiny mce

---
+++ webapp/resources/js/tinyScript.js
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+
+tinymce.init({
+	selector: "#contenidoNoticia",
+	plugins: "textcolor, table lists code",
+	toolbar: 'undo redo | styleselect | bold italic | link image | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | forecolor backcolor table code'
+});
