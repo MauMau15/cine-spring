c04ca3ca20f5085ea96b0bddcbe28849d62d8e43
MauMau15 <mauriciorenero@gmail.com>
2018-12-14 18:40:02 -0600

    adding noticias guardar method to get request by post

--- src/main/java/net/itinajero/app/controller/NoticiasController.java
+++ src/main/java/net/itinajero/app/controller/NoticiasController.java
@@ -2,7 +2,11 @@ package net.itinajero.app.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import net.itinajero.app.model.Noticia;
 
 @Controller
 @RequestMapping("/noticias")
@@ -10,6 +14,17 @@ public class NoticiasController {
 	
 	@GetMapping(value="/create")
 	public String crear() {
+		return "noticias/formNoticia";
+	}
+	
+	@PostMapping(value="/save")
+	public String guardar(@RequestParam("tituloNoticia") String titulo, @RequestParam("statusNoticia") String status, @RequestParam("contenidoNoticia") String contenido) {
+		Noticia noticia = new Noticia();
+		
+		noticia.setTitulo(titulo);
+		noticia.setEstatus(status);
+		noticia.setContenido(contenido);
+		
 		return "noticias/formNoticia";
 	}
 }
