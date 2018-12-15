3ee833da32cada7d1d4bfcc2c1a740698367f7a7
MauMau15 <mauriciorenero@gmail.com>
2018-12-14 18:41:24 -0600

    adding Noticia model

---
+++ src/main/java/net/itinajero/app/model/Noticia.java
@@ -0,0 +1,53 @@
+package net.itinajero.app.model;
+
+import java.util.Date;
+
+public class Noticia {
+
+	private int id;
+	private String titulo;
+	private String estatus;
+	private String contenido;
+	private Date fecha;
+	
+	public Noticia() {
+		fecha = new Date();
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getEstatus() {
+		return estatus;
+	}
+
+	public void setEstatus(String estatus) {
+		this.estatus = estatus;
+	}
+
+	public String getContenido() {
+		return contenido;
+	}
+
+	public void setContenido(String contenido) {
+		this.contenido = contenido;
+	}
+
+	@Override
+	public String toString() {
+		return "Noticia [id=" + id + ", titulo=" + titulo + ", estatus=" + estatus + ", contenido=" + contenido + "]";
+	}
+}
